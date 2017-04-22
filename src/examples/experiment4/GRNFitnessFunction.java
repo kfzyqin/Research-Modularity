207d0851871ddbbd88c3eed411aabbab58a6fa39
Zhenyue Qin <u5505995@anu.edu.au>
2017-04-22 15:59:21 +1000

    inital commit

---
+++ src/examples/experiment4/GRNFitnessFunction.java
@@ -0,0 +1,9 @@
+package examples.experiment4;
+
+/**
+ * Created by Zhenyue Qin on 22/04/2017.
+ * The Australian National University.
+ */
+public class GRNFitnessFunction {
+    
+}
---
+++ src/examples/experiment4/GRNMain.java
@@ -0,0 +1,24 @@
+package examples.experiment4;
+
+/**
+ * Created by Zhenyue Qin on 22/04/2017.
+ * The Australian National University.
+ */
+public class GRNMain {
+
+    private static final int size = 200;
+    private static final int maxGen = 2000;
+    private static final int numElites = 10;
+    private static final int tournamentSize = 2;
+    private static final double selectivePressure = 0.7;
+    private static final double mutationRate = 0.05;
+    private static final double crossoverRate = .8;
+    private static final double epsilon = .5;
+    private static final int maxFit = 32;
+    private static final String outfile = "Exp4.out";
+
+    public static void main(String[] args) {
+        // Fitness Function
+        
+    }
+}
