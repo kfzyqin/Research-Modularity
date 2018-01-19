42accec205f72d63862f81382f573a19e5da4112
Bob McKay <rimanucs@gmail.com>
2018-01-19 11:51:28 +1100

    Updating mkpdf.sh to not use a dvi file. This may or may not be compatible with ACM requirements (need to check by running through ACM validator)

--- papers/GECCO/mkpdf.sh
+++ papers/GECCO/mkpdf.sh
@@ -1,8 +1,13 @@
+# ./mkpdf.sh creates the pdf for an article. This version may work for ACM, but
+# confirmation is needed (note that at least until 2015, pdflatex was not
+# compatible with IEEE requirements)
+
 pdflatex mod
 pdflatex mod
 bibtex mod
 pdflatex mod
 pdflatex mod
-dvips -t letter -Ppdf mod.dvi -o
-ps2pdf -sPAPERSIZE=letter mod.ps
+# Don't need to go via dvi because using pdflatex???
+# dvips -t letter -Ppdf mod.dvi -o
+# ps2pdf -sPAPERSIZE=letter mod.ps
 
