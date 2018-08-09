217aa78491acfea0618232692c9beb0d1bce9d74
Zhenyue Qin <zhenyue.qin@anu.edu.au>
2018-08-09 10:48:09 +1000

    some changes

--- src/main/java/experiments/exp6_haploid_hotspot_aid_matrix_x_validation/HaploidGRNMatrixMain.java
+++ src/main/java/experiments/exp6_haploid_hotspot_aid_matrix_x_validation/HaploidGRNMatrixMain.java
@@ -48,7 +48,7 @@ public class HaploidGRNMatrixMain {
     /* Parameters of the GRN */
     private static final int maxCycle = 30;
     private static final int edgeSize = 20;
-    private static final int perturbations = 500;
+    private static final int perturbations = 100;
     private static final double perturbationRate = 0.15;
 
     /* Parameters of the GA */
@@ -67,7 +67,7 @@ public class HaploidGRNMatrixMain {
     /* Settings for text outputs */
     private static final String summaryFileName = "Summary.txt";
     private static final String csvFileName = "Statistics.csv";
-    private static final String outputDirectory = "soto";
+    private static final String outputDirectory = "all-combination-asymmetric-stride-p45";
     private static final String mainFileName = "HaploidGRNMatrixMain.java";
     private static final String allPerturbationsName = "Perturbations.per";
     private static final String modFitNamePrefix = "phenotypes";
@@ -78,18 +78,18 @@ public class HaploidGRNMatrixMain {
     private static final String plotTitle = "Haploid GRN Matrix";
     private static final String plotFileName = "Trends.png";
 
-    private static final double stride = 0.2;
+    private static final double stride = 0.45;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 //        int[][] targets = {target1, target2, target3, target4, target5, target6, target7};
         int[][] targets = {target1, target2};
 
         /* Fitness function */
-        FitnessFunction fitnessFunction = new GRNFitnessFunctionMultipleTargets(
-                targets, maxCycle, perturbations, perturbationRate, thresholds);
+//        FitnessFunction fitnessFunction = new GRNFitnessFunctionMultipleTargets(
+//                targets, maxCycle, perturbations, perturbationRate, thresholds);
 
-//        FitnessFunction fitnessFunction = new GRNFitnessFunctionMultipleTargetsAsymmetric(
-//                targets, maxCycle, perturbations, perturbationRate, thresholds, stride);
+        FitnessFunction fitnessFunction = new GRNFitnessFunctionMultipleTargetsAsymmetric(
+                targets, maxCycle, perturbations, perturbationRate, thresholds, stride);
 
 //        FitnessFunction fitnessFunction = new GRNFitnessFunctionMultipleTargetsFast(
 //                targets, maxCycle, perturbations, perturbationRate, thresholds, perturbationCycleSize);
