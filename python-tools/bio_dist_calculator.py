8943ff8edee19cf6c846fe6c333ebc5ede43bfa7
Zhenyue Qin <chin290956355@gmail.com>
2019-02-18 14:15:32 +1100

    Now I have understood where did the fitness of 0.9460 come from.

--- python-tools/bio_dist_calculator.py
+++ python-tools/bio_dist_calculator.py
@@ -14,13 +14,39 @@ def get_bio_dist_list(n, p):
     return bio_dist
 
 
+def get_dst_gamma(d, d_max):
+    gamma = math.pow((1.0 - (d/float(d_max))), 5)
+    return gamma
+
+
+def avg_gamma_formula(a_gamma):
+    return 1 - math.pow(math.e, (-3 * a_gamma))
+
+
 bio_dist_10 = get_bio_dist_list(10, 0.15)
 bio_dist_5 = get_bio_dist_list(5, 0.15)
 
+print('bio dist: ', bio_dist_5)
 
+an_n = 5
+max_gamma = get_dst_gamma(0, an_n)
+a_sum = 0
 
+sums = []
+for i in range(0, an_n+1):
+    if i < (an_n / 2.0):
+        sums.append(bio_dist_5[i] * get_dst_gamma(0, 5))
+        a_sum += bio_dist_5[i] * get_dst_gamma(0, 5)
+    else:
+        sums.append(bio_dist_5[i] * get_dst_gamma(5, 5))
+        a_sum += bio_dist_5[i] * get_dst_gamma(5, 5)
+        # sums.append(0)
+        # a_sum += 0
 
+print('a sum: ', a_sum)
+print('sums: ', sums)
 
+print(avg_gamma_formula(a_sum))
 
 
 
