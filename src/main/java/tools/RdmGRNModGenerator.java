7fa269c309b8ca714c7e4fccf3fca7509fa0743a
Zhenyue Qin <zhenyue.qin@anu.edu.au>
2019-07-31 15:45:38 +1000

    Start investigating mod shading.

--- src/main/java/tools/RdmGRNModGenerator.java
+++ src/main/java/tools/RdmGRNModGenerator.java
@@ -24,10 +24,10 @@ public class RdmGRNModGenerator {
         List<Double> maxMods = new ArrayList<>();
         List<Double> minMods = new ArrayList<>();
 
-        int[] ses = {15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+        int[] ses = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
         for (int aSz : ses) {
             List<Double> mods = new ArrayList<>();
-            for (int i = 0; i < 1000000; i++) {
+            for (int i = 0; i < 10000; i++) {
                 List<Integer> edgeIdxes = Randomness.generateASetOfIntegers(0, 99, aSz);
                 List<Integer> aGRN = new ArrayList<>(Collections.nCopies(100, 0));
                 for (Integer edgeIdx : edgeIdxes) {
