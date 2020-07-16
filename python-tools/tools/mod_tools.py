3f6def17510114a71967f14daa82cb787951fd93
Zhenyue Qin <zhenyue.qin@anu.edu.au>
2020-07-16 19:08:38 +1000

    Fixed the bug of normalising modularity q.

--- python-tools/tools/mod_tools.py
+++ python-tools/tools/mod_tools.py
@@ -4,10 +4,11 @@ import storage.mod_warehouse as mod_warehouse
 def normalize_mod_q(qs, edge_nums):
     normed_mod_qs = []
     for i in range(len(qs)):
-        an_edge_num_idx = edge_nums[i] - edge_nums[0]
-        max_q = mod_warehouse.max_mod_15_to_50[an_edge_num_idx]
-        min_q = mod_warehouse.min_mod_15_to_50[an_edge_num_idx]
-        avg_q = mod_warehouse.avg_mod_15_to_50[an_edge_num_idx]
+        an_edge_num_idx = edge_nums[i] - 1
+        assert an_edge_num_idx >= 0
+        max_q = mod_warehouse.max_mod_1_to_70[an_edge_num_idx]
+        min_q = mod_warehouse.min_mod_1_to_70[an_edge_num_idx]
+        avg_q = mod_warehouse.avg_mod_1_to_70[an_edge_num_idx]
         q = qs[i]
 
         normed_mod_qs.append((q - avg_q) / (max_q - avg_q))
