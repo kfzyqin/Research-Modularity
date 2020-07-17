1ae9c323dbca21b1c39e43c8dec4d29420628ef5
rouyi <u5920982@anu.edu.au>
2020-07-17 14:15:22 +1000

    extended filling operator

--- src/main/java/ga/operations/postOperators/ExtendedFillingOperator.java
+++ src/main/java/ga/operations/postOperators/ExtendedFillingOperator.java
@@ -23,13 +23,37 @@ public class ExtendedFillingOperator<T extends Chromosome> implements PostOperat
         this.selector = (ExtendedTournamentSelector) haploidSelector;
     }
 
-    @Override
-    public void postOperate(@NotNull Population<T> population) {
-        final int amount = population.getSize() - population.getNextGenSize();
-        List<T> individuals = selector.select(amount);
-        population.addCandidateChromosomes(individuals);
+//    @Override
+//    public void postOperate(@NotNull Population<T> population) {
+//        final int amount = population.getSize() - population.getNextGenSize();
+//        List<T> selected = new ArrayList<>();
+////        for (int i = 0; i<= amount/2; i ++) {
+////            List<T> individuals = selector.select(2);
+////            for (T individual: individuals){
+////                selected.add(individual);
+////            }
+////        }
+//
+//        List<T> individuals = selector.select(amount);
+//        population.addCandidateChromosomes(individuals);
+//
+//    }
 
+    @Override
+    public void postOperate(@NotNull final Population<T> population) {
+        Set<Integer> survivedIndices = population.getSurvivedIndicesView();
+        Set<Integer> selected = new HashSet<>();
+        List<Individual<T>> individuals = population.getIndividualsView();
+//        List<Double> normalized = normalizeFitness(individuals);
+        final int amount = population.getSize() - population.getNextGenSize();
+
+        while (selected.size() < amount) {
+            final int index = selector.selectIndividual();
+            if (!survivedIndices.contains(index)) selected.add(index);
+        }
+        for (Integer i : selected) population.addCandidate(individuals.get(i).copy());
     }
+
 
 
 }
